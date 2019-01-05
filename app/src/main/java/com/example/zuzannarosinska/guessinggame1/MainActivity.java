29ca5b3cc7e55abebe10ba8722f0581deca552dd
zuzanna.rosinska <zurosin@gmail.com>
2019-01-05 20:34:13 +0100

    Workable version

--- app/src/main/java/com/example/zuzannarosinska/guessinggame1/MainActivity.java
+++ app/src/main/java/com/example/zuzannarosinska/guessinggame1/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,17 +17,32 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtGuess;
     private Button btnGuess;
     private TextView lblOutput;
+
+    private int theNumber;
     public void checkGuess(){
         String guessText = txtGuess.getText().toString();
         String message = "";
-        try{
+        try {
             int guess = Integer.parseInt(guessText);
             if (guess < theNumber)
                 message = guess + " is too low. Try again.";
-
-        }
+            else if (guess > theNumber)
+                message = guess + " is too high. Try again.";
+            else {
+                message = guess + " is correct. You win! Let's play the game!";
+                newGame();
+            }
+        }catch (Exception e) {
+            message = "Enter a WHOLE NUMBER between 1 and 100.";
+        }finally {
+            lblOutput.setText(message);
+            txtGuess.requestFocus();
+            txtGuess.selectAll();
+            }
     }
-
+        public void newGame(){
+        theNumber=(int)(Math.random()*100+1);
+        }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +51,18 @@ public class MainActivity extends AppCompatActivity {
         txtGuess = (EditText) findViewById(R.id.txtGuess);
         btnGuess = (Button) findViewById(R.id.btnGuess);
         lblOutput = (TextView) findViewById(R.id.lblOutput);
+        newGame();
+        btnGuess.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                checkGuess();
+            }
+        });
+        txtGuess.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                checkGuess();
+                return true;
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
