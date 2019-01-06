82ecd5a2d0e15fccafa2218e675b31ba7cbcc355
xkadmx <zuzanna.rosinska@yahoo.com>
2019-01-06 20:49:31 +0100

    Menu bar added

--- app/src/main/java/com/example/zuzannarosinska/guessinggame1/MainActivity.java
+++ app/src/main/java/com/example/zuzannarosinska/guessinggame1/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -50,7 +51,9 @@ public class MainActivity extends AppCompatActivity {
 
         txtGuess = (EditText) findViewById(R.id.txtGuess);
         btnGuess = (Button) findViewById(R.id.btnGuess);
-        lblOutput = (TextView) findViewById(R.id.lblOutput);
+        lblOutput = /*(TextView)*/ findViewById(R.id.lblOutput);
+
+
         newGame();
         btnGuess.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
@@ -80,6 +83,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -98,4 +103,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
